638c8481076529cb50bfb5c34ececa835385fad7
Corne2Plum3 <corne2plum3@gmail.com>
2024-09-28 16:45:46 +0200

    Build SC fonts using pyftfeatfreeze outside of gftools builder

--- sources/build_fonts.sh
+++ sources/build_fonts.sh
@@ -7,9 +7,12 @@ COMPILATION_START_TIME=$(date +%s)  # when the compilation started
 cd sources/
 
 FONTS_DIR="../fonts"
+FONTS_DIR_LIST=(otf ttf variable webfonts)  # all directories in FONTS_DIR
+FONTS_TYPE_LIST=(otf ttf ttf woff2)  # type of the file of each folder defined above
 UFO_DIR="ufo/"  # where the ufo source files are (don't forget the '/' at the end!)
 UFO_LIST=$(ls $UFO_DIR)  # list of all files inside UFO_DIR
 FONTNAME="Giphurs"
+
 
 # Applying some change to ufo file
 echo -e "\x1b[0;36mApplying some changes to the ufo sources.\x1b[0;0m"
@@ -24,20 +27,43 @@ done
 
 echo "Done editing UFO files."
 
-# Build the font (the touch command is needed otherwhise I dunno why some non-encoded glyphs don't get updated...
+# Build the font (the touch command is needed otherwhise I dunno why some non-encoded glyphs don't get updated)...
 echo -e "\x1b[0;36mBuilding the fonts\x1b[0;0m"
 touch Giphurs.designspace && gftools builder config.yaml
 
 # Fix incorrect name of the weight 1000
 WEIGHT_1000_OLD_NAME="Giphurs-ExtraBlack"
 WEIGHT_1000_NEW_NAME="GiphursExtraBlack-Regular"
-WEIGHT_1000_SC_OLD_NAME="GiphursSC-ExtraBlack"
-WEIGHT_1000_SC_NEW_NAME="GiphursSCExtraBlack-Regular"
 mv $FONTS_DIR/otf/$WEIGHT_1000_OLD_NAME.otf $FONTS_DIR/otf/$WEIGHT_1000_NEW_NAME.otf 
 mv $FONTS_DIR/ttf/$WEIGHT_1000_OLD_NAME.ttf $FONTS_DIR/ttf/$WEIGHT_1000_NEW_NAME.ttf 
-mv $FONTS_DIR/webfonts/$WEIGHT_1000_OLD_NAME.woff2 $FONTS_DIR/webfonts/$WEIGHT_1000_NEW_NAME.woff2 
-mv $FONTS_DIR/otf/$WEIGHT_1000_SC_OLD_NAME.otf $FONTS_DIR/otf/$WEIGHT_1000_SC_NEW_NAME.otf 
-mv $FONTS_DIR/ttf/$WEIGHT_1000_SC_OLD_NAME.ttf $FONTS_DIR/ttf/$WEIGHT_1000_SC_NEW_NAME.ttf 
+mv $FONTS_DIR/webfonts/$WEIGHT_1000_OLD_NAME.woff2 $FONTS_DIR/webfonts/$WEIGHT_1000_NEW_NAME.woff2
+
+# Build SC variants of the fonts
+for (( i=0; i<4; i++));
+do
+    font_sub_dir=${FONTS_DIR_LIST[$i]}
+    font_type=${FONTS_TYPE_LIST[$i]}
+    font_dir_full="${FONTS_DIR}/${font_sub_dir}"
+    font_list=$(ls $font_dir_full)
+    for font in $font_list
+    do
+        font_extension=$(echo $font | cut -d"." -f2)
+        sc_font_file_name=""
+        if [[ $font =~ "[" ]]
+        then
+            font_name=$(echo $font | cut -d"[" -f1)
+            font_axis=$(echo $font | cut -d"[" -f2 | cut -d"]" -f1)
+            sc_font_file_name="${font_name}SC[${font_axis}].${font_extension}"
+        else
+            font_name=$(echo $font | cut -d"-" -f1)
+            font_weight=$(echo $font | cut -d"-" -f2)
+            sc_font_file_name="${font_name}SC-${font_weight}"
+        fi
+        pyftfeatfreeze -f "smcp" -S -U "SC" $font_dir_full/$font $font_dir_full/$sc_font_file_name
+        echo "Done building $sc_font_file_name"
+    done
+    wait
+done
 
 # Add hinting
 echo -e "\x1b[0;36mAdd hinting on font binaries\x1b[0;0m"
@@ -67,6 +93,7 @@ cd ../ttf
 ls | grep backup | xargs rm
 cd ../webfonts
 ls | grep backup | xargs rm
+echo "Done."
 
 # Go back to where we come from
 cd ../..
